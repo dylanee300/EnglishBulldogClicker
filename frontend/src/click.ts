56e173495cc14b5a501adb7f525c16b558abbc00
dylanee300 <dylanee300@gmail.com>
2025-07-30 14:00:51 -0700

    added null checks and more

--- frontend/src/click.ts
+++ frontend/src/click.ts
@@ -1,12 +1,21 @@
-const doggie = document.getElementById("doggie") as HTMLImageElement;
-const display = document.getElementById("doggies") as HTMLSpanElement;
+const doggie = document.getElementById("doggie") as HTMLImageElement | null;
+const display = document.getElementById("doggies") as HTMLSpanElement | null;
+
+if (!doggie || !display) {
+  throw new Error("Required elements doggie or doggies not found in the dom");
+}
 
 let count: number = parseInt(localStorage.getItem("doggieCount") || "0", 10);
-let dpc:number = parseInt(localStorage.getItem("dpc") || "1", 10);
+let dpc: number = parseInt(localStorage.getItem("dpc") || "1", 10);
+
+function saveData(): void {
+  localStorage.setItem("doggieCount", count.toString());
+  localStorage.setItem("dpc", dpc.toString());
+}
 
 function updateDisplay(): void {
   display.textContent = count.toString();
-  localStorage.setItem("doggieCount", count.toString());
+  saveData();
 }
 
 updateDisplay();
@@ -20,7 +29,7 @@ doggie.addEventListener("click", () => {
 // upgrades
 
 
-const upgradesBtn = document.getElementById("u1") as HTMLButtonElement;
+const upgradesBtn = document.getElementById("u1") as HTMLImageElement;
 upgradesBtn.addEventListener("click", () => {
   if (count >= 50) {
     count -= 50;
@@ -30,7 +39,7 @@ upgradesBtn.addEventListener("click", () => {
   }
 });
 
-const upgradesBtn2 = document.getElementById("u2") as HTMLButtonElement;
+const upgradesBtn2 = document.getElementById("u2") as HTMLImageElement;
 upgradesBtn2.addEventListener("click", () => {
   if (count >= 500) {
     count -= 500;
@@ -40,3 +49,42 @@ upgradesBtn2.addEventListener("click", () => {
   }
 });
 
+const upgradesBtn3 = document.getElementById("u3") as HTMLImageElement;
+upgradesBtn3.addEventListener("click", () => {
+  if (count >= 1750) {
+    count -= 1750;
+    dpc += 10; 
+    localStorage.setItem("dpc", dpc.toString()); 
+    updateDisplay();
+  }
+});
+
+const upgradesBtn4 = document.getElementById("u4") as HTMLImageElement;
+upgradesBtn3.addEventListener("click", () => {
+  if (count >= 4000) {
+    count -= 4000;
+    dpc += 25; 
+    localStorage.setItem("dpc", dpc.toString()); 
+    updateDisplay();
+  }
+});
+
+const upgradesBtn5 = document.getElementById("u5") as HTMLImageElement;
+upgradesBtn3.addEventListener("click", () => {
+  if (count >= 10000) {
+    count -= 10000;
+    dpc += 50; 
+    localStorage.setItem("dpc", dpc.toString()); 
+    updateDisplay();
+  }
+});
+
+const upgradesBtn6 = document.getElementById("u6") as HTMLImageElement;
+upgradesBtn6.addEventListener("click", () => {
+  if (count >= 30000) {
+    count -= 30000;
+    dpc += 250; 
+    localStorage.setItem("dpc", dpc.toString()); 
+    updateDisplay();
+  }
+});
