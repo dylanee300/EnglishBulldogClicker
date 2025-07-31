84b769928134bdd59acb70bc4e9131752ddd5b5b
dylanee300 <dylanee300@gmail.com>
2025-07-31 07:43:42 -0700

    added back upgrades thing

--- frontend/src/upgrades/upgrades.ts
+++ frontend/src/upgrades/upgrades.ts
@@ -6,9 +6,21 @@ export function setupUpgrades(
 ) {
 
 
-    
+
 // upgrades
 
+/* const upgrades = [
+    {id: 'u1', cost: 50, dpc: 1},
+    {id: 'u2', cost: 500, dpc: 5},
+    {id: 'u3', cost: 1750, dpc: 10},
+    {id: 'u4', cost: 4000, dpc: 25},
+    {id: 'u5', cost: 10000, dpc: 50},
+    {id: 'u6', cost: 30000, dpc: 250},
+    {id: 'u7', cost: 75000, dpc: 750},
+    {id: 'u8', cost: 225000, dpc: 3000},
+    {id: 'u9', cost: 2000000, dpc: 10000},
+    {id: 'u10', cost: 50, dpc: 1}
+] */
 
 
   const upgradesBtn = document.getElementById("u1") as HTMLImageElement;
