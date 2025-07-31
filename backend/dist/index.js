c285c978e7c8ea186a94f042a40f12e8f358db0a
dylanee300 <dylanee300@gmail.com>
2025-07-31 07:05:56 -0700

    stop tracking all dist folders

--- backend/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var app = (0, express_1.default)();
-var PORT = 3000;
-app.listen(PORT, function () {
-    console.log("Server is running on http://localhost:".concat(PORT));
-});
