979855aa93f68033531bdb201699ea9dcf1be06b
dylanee300 <dylanee300@gmail.com>
2025-08-12 07:30:00 -0700

    started clicker page

--- src/pages/Click/Click.jsx
+++ src/pages/Click/Click.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import './Click.css';
+import Doggie from '../../assets/EnglishBullDog.png';
 
-function Click() {
-    return <div>Click Placeholder</div>;
+const doggie = <img src={Doggie} alt="" />;
+
+
+let count = 0;
+
+
+function click() {
+    return (
+        <div>
+            <img src={Doggie} id="clickerDoggie"></img>
+        </div>
+
+    )
 }
 
-export default Click;
+export default click;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function NotFound() {
-    return <h1>404 - Not Found</h1>;
+    return (
+        <div>
+            <h1>404 - Not Found.</h1>
+            <h3>We tried our best to find the page you were looking for but we couldnt find it.</h3>
+        </div>
+    )
 }
 
 export default NotFound;
