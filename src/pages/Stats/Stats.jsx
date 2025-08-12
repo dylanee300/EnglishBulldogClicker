0aca7f60afc056a62427fea2d8088b3db9b21310
dylanee300 <dylanee300@gmail.com>
2025-08-12 07:07:48 -0700

    done with the start of stats page.. will work more on it later today

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import './Stats.css';
 import Doggie from '../../assets/EnglishBullDog.png';
 
+
+let user = "dylanee" // for now til i add acounts and stuff then wel use user data..
+let doggiesUnlocked = 3;
+let doggies = 4000;
+let doggiesBought = 3;
+let dpc = 100;
+
 function Stats() {
     return (
         <div>
@@ -10,6 +17,22 @@ function Stats() {
             <div className="pfp">
                 <img src={Doggie} alt="" draggable="false" id="pfpdoggie"/>
             </div>
+
+            <div className="user">
+                <div>{user}</div>
+            </div>
+
+            <div className="userStats">
+
+                <div className="doggiesUnlock">{/* for later */}</div>
+
+                <div className="doggies">Doggies: {doggies}</div>
+
+                <div className="doggiesBought">{/* for later */}</div>
+
+                <div className="dpc">DPC: {dpc}</div>
+            </div>
+
         </div>
     )
 }
