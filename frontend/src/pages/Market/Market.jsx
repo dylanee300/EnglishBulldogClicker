e0ecf0c7688c2c7842519cae107571850dd684c2
dylanee300 <dylanee300@gmail.com>
2025-08-15 08:49:09 -0700

    added some dogs to the market.. also changed one of the main dogs but will most likley go back to the other one

--- frontend/src/pages/Market/Market.jsx
+++ frontend/src/pages/Market/Market.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import doggie1 from '../../assets/alttdoggie.png';
+import doggie2 from '../../assets/EnglishBullDog.png';
+import './Market.css'; 
 
+
+// for now I'm just going to import all the doggies then we can deal with the rest later..
 function Market() {
     return <div>
-        
+        <h1 id="title">Market</h1>
+        <div className="shopDoggies">
+            <img src={doggie1} id="doggie1" />
+            <img src={doggie2} id="doggie2" />
+        </div>
     </div>;
 }
 
